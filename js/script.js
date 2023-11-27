8c04cb3811dfed1cf84f1a349c9d47dc97a35b09
katolex <147526315+katolex@users.noreply.github.com>
2023-11-27 16:29:44 -0700

    Changing to button tag

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 console.log("Hello World");
+
 var year = new Date().getFullYear();
 var date = '&copy; studio.of.kp {year}. All Rights Reserved.';
 document.getElementById("rights").innerHTML = year;
 
+const button = document.getElementById("button")
+button.addEventListener("click", function(e) {
+    const name = prompt("Hello World");
+    console.log(name);
+});
