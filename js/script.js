b20720865bc45552c18514be2695379d3f3f8a4f
katolex <147526315+katolex@users.noreply.github.com>
2023-11-27 18:48:09 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ var year = new Date().getFullYear();
 var date = '&copy; studio.of.kp {year}. All Rights Reserved.'; 
 document.getElementById("rights").innerHTML = year;
 
-function myFunction = () => {
-    alert("Hello world!")
+const buttonalert = () => {
+    alert("Hello world!");
 }
 
 document.getElementById('btn-alert').onmouseover = () => {
