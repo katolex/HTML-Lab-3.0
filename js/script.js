15dc7207ce0e91b325d90a38621fe0ca5ff535d5
katolex <147526315+katolex@users.noreply.github.com>
2023-12-04 18:37:55 -0700

    Adding loop and finishing increment button

--- js/script.js
+++ js/script.js
@@ -1,23 +1,53 @@
 console.log("Hello World");
 
-var year = new Date().getFullYear();
-var date = '&copy; studio.of.kp {year}. All Rights Reserved.'; 
+//footer
+let year = new Date().getFullYear();
+console.log("sanity check: " + year);
+let date = '&copy; studio.of.kp' + year + '. All Rights Reserved.'; 
 document.getElementById("rights").innerHTML = year;
 
-function buttonalert () {
+const buttonalert = () => {
     alert("Hello world!");
 }
 
-function clickme() {
+const clickme = () => {
 document.getElementById("btn-alert").innerHTML = "YES CLICK ON ME!";
 }
-function clickhere() {
+const clickhere = () => {
     document.getElementById("btn-alert").innerHTML = "Click here!";
-    }
+}
 
-    var btnclick = 0;
+//incrementing button
+let btnclick = 0;
+let text = document.getElementById("counter");
+document.getElementById("btn-counter").onclick = () => {
+    btnclick += 1;
+    text.innerHTML = btnclick;
+    console.log("sanity check: " + btnclick);   
 
-    function clickplus() {
-        btnclick += 1;
-        document.getElementById("counter").innerHTML = "" + btnclick;
+    if (btnclick%2 == 0){
+        text.classList.remove("odd");
+        text.classList.add("even");
     }
+    else{
+        text.classList.remove("even");
+        text.classList.add("odd");
+    }
+}
+
+
+//for loop
+for (let i = 1; i <= 100; i++) {
+    const node = document.createElement("li");
+    let evenOrOdd;
+    if (i%2 == 0){
+        node.classList.add('even');
+        evenOrOdd = "even";
+    }
+    else {
+        node.classList.add('odd');
+        evenOrOdd = "odd";
+    }
+    node.textContent = evenOrOdd;
+    document.getElementById("numbers").appendChild(node);
+}
